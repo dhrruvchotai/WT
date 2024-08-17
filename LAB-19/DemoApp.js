5381f00493ddcf04c2b703c0e97199948e8d857a
dhrruvchotai <23010101051@darshan.ac.in>
2024-08-17 19:48:01 +0530

    Till 17-08

--- LAB-19/DemoApp.js
+++ LAB-19/DemoApp.js
@@ -1,23 +1,56 @@
+// const express = require('express');
+// const app = express();
+
+// app.listen(3200,() => {
+//     console.log("Server started!!");
+// });
+
+
+// app.get('/home',(req,res) => {
+//     res.sendFile(__dirname+"/DemoApp.html");
+// });
+
+// app.get('/about',(req,res) => {
+//     res.sendFile(__dirname+"/DemoApp.html");
+// });
+
+// app.get('/contact',(req,res) => {
+//     res.sendFile(__dirname+"/DemoApp.html");
+// });
+
+// app.get('/', (req,res) => {
+//     res.redirect('/home');
+// });
+
+
 const express = require('express');
+const fs = require('fs');
 const app = express();
 
-app.listen(3200,() => {
-    console.log("Server started!!");
+const port = 3400;
+
+app.get('/home', (req,res)=>{
+    res.setHeader('Content-Type', 'text/plain');
+    fs.readFile('home.txt', (err,data)=>{ 
+        res.send(data.toString())        
+    })
+});
+
+app.get('/about', (req,res)=>{
+    res.setHeader('Content-Type', 'text/plain');
+    fs.readFile('about.txt', (err,data)=>{
+        res.send(data.toString())
+    })
+});
+
+app.get('/contact', (req,res)=>{
+    res.setHeader('Content-Type', 'text/plain'); 
+    fs.readFile('contact.txt', (err,data)=>{
+    res.send(data.toString())
+    })
 });
 
 
-app.get('/home',(req,res) => {
-    res.sendFile(__dirname+"/DemoApp.html");
-});
-
-app.get('/about',(req,res) => {
-    res.sendFile(__dirname+"/DemoApp.html");
-});
-
-app.get('/contact',(req,res) => {
-    res.sendFile(__dirname+"/DemoApp.html");
-});
-
-app.get('/', (req,res) => {
-    res.redirect('/home');
-});
+app.listen(port,()=>{
+    console.log("done") 
+})
